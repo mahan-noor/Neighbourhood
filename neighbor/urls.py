f903f891004a5c287bbf72bbfcb75ec4f6abb554
mahan-noor <ismahanabey@gmail.com>
2021-06-04 11:21:53 +0300

    add path url for class business and neighbors and the method request for them

--- neighbor/urls.py
+++ neighbor/urls.py
@@ -9,5 +9,8 @@ urlpatterns=[
   path('api/users/',views.UserList.as_view(),name='users'),
   path('api/users/update/<int:pk>/',views.UserList.as_view(),name='update_users'),
   path('api/users/delete/<int:pk>/',views.UserList.as_view(),name='delete_users'),
-  
+  re_path('api/update/business/(?P<pk>[0-9]+)/',views.BusinessList.as_view(),name='update_business'),
+  path('api/delete/business/<int:pk>/',views.BusinessList.as_view(),name='delete_business'),
+  path('api/update/neighbors/<int:pk>/',views.NeighborhoodList.as_view(),name='update_neighbors'),
+  re_path('api/delete/(?P<pk>[0-9]+)/',views.NeighborhoodList.as_view(),name='delete_neighbors')
 ]
